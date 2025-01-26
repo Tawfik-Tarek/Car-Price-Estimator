27b590a3d390522e39fe757611b6538c8f9cb3e9
Tawfik-Tarek <tawfiktarek1975@gmail.com>
2025-01-26 06:32:55 +0200

    feat: Implement update and remove user methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -32,4 +32,23 @@ export class UsersService {
     return user;
   }
 
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    Object.assign(user, attrs);
+    return this.repo.save(user)
+  }
+
+  async remove(id: number){
+    const user = await this.findOne(id);
+    if (! user) {
+        throw new Error("User not found")
+    }
+
+    return this.repo.remove(user)
+  }
 }
