4dae5d4f094d5abf5786218d2f68a62bfaff5a97
Tawfik-Tarek <tawfiktarek1975@gmail.com>
2025-01-28 08:44:07 +0200

    make interface to accept dto in case it is a class instead of type any

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,17 +1,25 @@
-import { CallHandler, ExecutionContext, NestInterceptor, UseInterceptors } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
 import { plainToClass, plainToInstance } from 'class-transformer';
 import { map, Observable } from 'rxjs';
 
 type InteceptType = Observable<any> | Promise<Observable<any>>;
 
-export function Serialize(dto: any){
-  return UseInterceptors(new SerializeInterceptor(dto))
+interface ClassInterface {
+  new (...args: any[]): {};
+}
+
+export function Serialize(dto: ClassInterface) {
+  return UseInterceptors(new SerializeInterceptor(dto));
 }
 
 export class SerializeInterceptor implements NestInterceptor {
-
-  constructor(private dto: any){
-    this.dto = dto
+  constructor(private dto: ClassInterface) {
+    this.dto = dto;
   }
 
   intercept(context: ExecutionContext, handler: CallHandler): InteceptType {
