982cfefca748a0b38808c28129b562e223d43fd8
Tawfik-Tarek <tawfiktarek1975@gmail.com>
2025-01-28 08:21:49 +0200

    make Serialize interceptor global on the controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,7 +13,7 @@ import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
-
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
   constructor(private UsersService: UsersService) {}
@@ -24,13 +24,11 @@ export class UsersController {
     this.UsersService.create(email, name, password);
   }
 
-  @Serialize(UserDto)
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.UsersService.findOne(parseInt(id));
   }
 
-  @Serialize(UserDto)
   @Get()
   findAllUsers(@Query('name') name: string) {
     return this.UsersService.find(name);
